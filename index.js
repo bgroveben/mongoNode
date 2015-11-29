388b7b1e4bc9ef06916e9d287cbe7f3dfe0168f1
bgrove1974 <bgroveben@gmail.com>
2015-11-29 01:28:33 -0800

    Insert Data

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+// NodeJS Driver
 // Declare MongoClient variable and other variables
+
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
-/* Connect using the MongoClient to a running mongod instance by specifying the MongoDB uri.
-   The following code connects to a MongoDB instance that runs on the localhost interface
-   on port 27017 and switches to the test database.*/
+//  Connect using the MongoClient to a running mongod instance by specifying the MongoDB uri.
+//  The following code connects to a MongoDB instance that runs on the localhost interface
+//  on port 27017 and switches to the test database.
+
 var url = 'mongodb://localhost:27017/test';
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
