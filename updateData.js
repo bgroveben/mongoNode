870cc3cb3bbcd39e1f795f7ccecf71a9087de3b0
bgrove1974 <bgroveben@gmail.com>
2015-12-02 00:16:55 -0800

    Update Fields

--- updateData.js
+++ updateData.js
@@ -15,3 +15,52 @@ var url = 'mongodb://localhost:27017/test';
 
 
 // Update Specific Fields
+/*
+To change a field value, MongoDB provides update operators, such as $set to modify values.
+Some update operators, such as $set, will create the field if the field does not already exist.
+*/
+// Update Top-Level Fields
+/*
+The following operation updates the first document with name equal to "Juni", using the $set operator
+to update the cuisine field and the $currentDate operator to update the lastModified field with
+the current date.
+Define an updateRestaurants function as follows:
+*/
+var updateRestaurants = function(db, callback) {
+  db.collection('restaurants').updateOne(
+    { "name" : "Juni" },
+    {
+      $set: { "cuisine" : "American (New)" },
+      $currentDate: { "lastModified" : true }
+    }, function(err, results) {
+      console.log(results);
+      callback();
+    });
+};
+// Then call the updateRestaurants function:
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  updateRestaurants(db, function() {
+    db.close();
+  });
+});
+
+
+// Update an Embedded Field
+// The following updates the street field in the embedded address document:
+var updateRestaurants = function(db, callback) {
+  db.collection('restaurants').updateOne(
+    { "restaurant_id" : "41156888" },
+    { $set: { "address.street" : "East 31st Street" } },
+    function(err, results) {
+      console.log(results);
+      callback();
+  });
+};
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  updateRestaurants(db, function() {
+    db.close();
+  });
+});
