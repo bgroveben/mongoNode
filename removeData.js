60d0e66e72d1f7957e40a33002b47d213ca73ab4
bgrove1974 <bgroveben@gmail.com>
2015-12-05 00:46:58 -0800

    Remove stuff

--- removeData.js
+++ removeData.js
@@ -9,3 +9,65 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/test';
+
+
+// Remove all documents that match a condition:
+// The following operation removes all documents that match the specified condition -- GOT IT?
+var removeRestaurants = function(db, callback) {
+  db.collection('restaurants').deleteMany(
+    { "borough": "Manhattan" },
+    function(err, results) {
+      console.log(results);
+      callback();
+    }
+  );
+};
+// Call the removeRestaurants function:
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  removeRestaurants(db, function() {
+    db.close();
+  });
+});
+
+
+// Remove just one document.
+/*
+By default, the deleteOne method removes all documents that match the remove condition.
+Use the justOne option to limit the remove operation to only one of the matching documents.
+The above statements are kind of confusing -- there is no justOne option in the docs or in the code below,
+and when the code runs, it removes only one element...WTF?
+*/
+var removeRestaurants = function(db, callback) {
+  db.collection('restaurants').deleteOne(
+    { "borough": "Queens" },
+    function(err, results) {
+      console.log(results);
+      callback();
+    }
+  );
+};
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  removeRestaurants(db, function() {
+    db.close();
+  });
+});
+
+
+// Remove All Documents
+// To remove all of the documents from a collection, pass an empty conditions document {} to the deleteMany method:
+var removeRestaurants = function(db, callback) {
+   db.collection('restaurants').deleteMany( {}, function(err, results) {
+      console.log(results);
+      callback();
+   });
+};
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  removeRestaurants(db, function() {
+    db.close();
+  });
+});
